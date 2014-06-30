42deb67e18c03754cd396dfb9b84186a3be21b00
Seb Bacon <seb.bacon@gmail.com>
2014-06-30 15:29:31 +0100

    Check no unpushed git commits before publishing gem

--- publish.sh
+++ publish.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-gem build turbot_ruby_gems.gemspec
-gem push $(ls *gem|tail -1)
+UNPUSHED=$(git log origin/master..HEAD)
+if [ "x$UNPUSHED" == "x" ]; then
+    gem build turbot_ruby_gems.gemspec
+    gem push $(ls *gem|tail -1)
+fi
 
 function clean {
  rm *gem
